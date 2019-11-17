3f74619fdbfe5aac3822fca4994d778b8b7db321
Craig <craig.holder@ontariotechu.net>
2019-11-16 22:49:37 -0500

    Ataack apears infront bitch

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -61,9 +61,12 @@ void PlayScene::KeyboardInput(GLFWwindow* window, glm::vec2 mousePos, int player
 	if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS)
 	{
 		attacks.push_back(new Attack(1));
-
-		//attacks.back()->ABox->SetPosition(players[0]->GetPosition());
-		attacks.back()->ABox->SetPosition(glm::vec3(0,2,4));
+		glm::vec3 p1 = glm::vec3(0.0f, 0.0f, 0.0f);
+		p1.x += 2 * cos(glm::radians((players[0]->GetTransform().rotation.y)));
+		p1.z += 2 * -sin(glm::radians((players[0]->GetTransform().rotation.y)));
+		p1.y = players[0]->GetPosition().y;
+		attacks.back()->ABox->SetPosition(p1);
+		//attacks.back()->ABox->SetPosition(glm::vec3(0,2,4));
 	}
 
 

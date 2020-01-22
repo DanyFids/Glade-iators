69e4c4edf5b78d380cea6f632405bfadcf1bc0f3
Futurez <anthony.ona@ontariotechu.net>
2020-01-22 12:26:43 -0500

    WIP - SAT Hitdetection
    I don't know what it means to get edgeDirs need help

--- src/Hitbox.cpp
+++ src/Hitbox.cpp
@@ -3,6 +3,8 @@
 
 CubeHitbox::CubeHitbox(float w, float h, float d):dim(glm::vec3(w,h,d))
 {
+	
+
 }
 
 bool CubeHitbox::HitDetect(Transform t, CubeHitbox* other, Transform oT) // AABB
@@ -47,6 +49,78 @@ bool CubeHitbox::HitDetect(Transform t, SphereHitbox* other, Transform oT)
 bool CubeHitbox::HitDetect(Transform t, CapsuleHitbox* other, Transform oT)
 {
 	return false;
+}
+
+//////////////// 
+//			For SAT Hit detection
+/////////////
+
+void CubeHitbox::setVerts(Transform t)
+{
+	vertices.clear();
+
+	float halfW = GetDim().x / 2 * t.scale.x;
+	float halfH = GetDim().y / 2 * t.scale.y;
+	float halfD = GetDim().z / 2 * t.scale.z;
+
+	vertices.push_back(glm::vec3(t.position.x + halfW, t.position.y + halfH, t.position.z + halfD) * t.GetRot); //right, top, front , 0 * (norm 1)
+	vertices.push_back(glm::vec3(t.position.x - halfW, t.position.y + halfH, t.position.z + halfD) * t.GetRot); //left, top, front , 1 * (norm 2)
+
+	vertices.push_back(glm::vec3(t.position.x + halfW, t.position.y + halfH, t.position.z - halfD) * t.GetRot); //right, top, back , 2
+	vertices.push_back(glm::vec3(t.position.x - halfW, t.position.y + halfH, t.position.z - halfD) * t.GetRot); //left, top, back , 3 * (norm 1) (norm3)
+
+	vertices.push_back(glm::vec3(t.position.x + halfW, t.position.y - halfH, t.position.z + halfD) * t.GetRot); //right, bottom, front , 4
+	vertices.push_back(glm::vec3(t.position.x - halfW, t.position.y - halfH, t.position.z + halfD) * t.GetRot); //left, bottom, front , 5
+
+	vertices.push_back(glm::vec3(t.position.x + halfW, t.position.y - halfH, t.position.z - halfD) * t.GetRot); //right, bottom, back , 6 (norm 3)
+	vertices.push_back(glm::vec3(t.position.x - halfW, t.position.y - halfH, t.position.z - halfD) * t.GetRot); //left, bottom, back , 7(norm 2)
+
+
+}
+
+void CubeHitbox::setNorms()
+{
+	normals.clear();
+
+	normals.push_back(glm::cross(vertices[0], vertices[3]));
+	normals.push_back(glm::cross(vertices[2], vertices[7]));
+	normals.push_back(glm::cross(vertices[6], vertices[3]));
+}
+
+void CubeHitbox::GetInterval(Transform t, glm::vec3 axis, float& min, float& max)
+{
+	setVerts(t);
+
+	min = max = glm::dot(axis, vertices[0]);
+	for (int i = 0; i < vertices.size(); i++) {
+		float value = glm::dot(axis, vertices[i]);
+		min = glm::min(min, value);
+		max = glm::max(max, value);
+	}
+
+}
+
+bool CubeHitbox::testIntersection(Transform t, CubeHitbox* object2, Transform oT)
+{
+	float min1, max1, min2, max2;
+
+	for (int i = 0; this->normals.size(); i++) {
+		GetInterval(t, this->normals[i], min1, max1);
+		GetInterval(oT, this->normals[i], min2, max2);
+		if (max1 < max2 || max2 < min1) {
+			return false;
+		}
+	}
+
+	for (int i = 0; object2->normals.size(); i++) {
+		GetInterval(t, object2->normals[i], min1, max1);
+		GetInterval(oT, object2->normals[i], min2, max2);
+		if (max1 < max2 || max2 < min1) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 /********************************************************************
--- src/Hitbox.h
+++ src/Hitbox.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<GLM/glm.hpp>
+#include<vector>
 
 struct Transform;
 
@@ -17,14 +18,30 @@ public:
 
 class CubeHitbox : public Hitbox {
 	glm::vec3 dim;
+
+	std::vector<glm::vec3> vertices;
+	std::vector<glm::vec3> normals;
+
+	//Temporary min-max ranges for SAT collision detection
+	float min;
+	float max;
 public:
 	CubeHitbox(float w, float h, float d);
 
 	glm::vec3 GetDim() {return dim;}
 
+
 	virtual bool HitDetect(Transform t, CubeHitbox* other, Transform oT);
 	virtual bool HitDetect(Transform t, SphereHitbox* other, Transform oT);
 	virtual bool HitDetect(Transform t, CapsuleHitbox* other, Transform oT);
+	virtual bool HitDetectSAT(Transform t, CubeHitbox* other, Transform oT);
+
+	virtual void setVerts(Transform t);
+	virtual void setNorms();
+	
+	virtual void GetInterval(Transform t, glm::vec3 axis, float& min, float& max);
+
+	virtual bool testIntersection(Transform t, CubeHitbox* object2, Transform oT);
 };
 
 class SphereHitbox : public Hitbox {

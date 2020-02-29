ae94a99d40a2ef8fbcf4fefa1a3203e4db2a58df
Craig <craig.holder@ontariotechu.net>
2020-02-29 16:47:28 -0500

    yeet idk

--- src/Text.cpp
+++ src/Text.cpp
@@ -1,17 +1,73 @@
 #include "Text.h"
+#include <glad\glad.h>
 
+TextRenderer::TextRenderer(bool Bold)
+{
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
 
+	FT_Face face;
+	if (FT_New_Face(ft, "Fonts/Aetherius.ttf", 0, &face))
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
 
+	FT_Set_Pixel_Sizes(face, 0, 48);
 
+	if (FT_Load_Char(face, 'X', FT_LOAD_RENDER))
+		std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
 
+	std::map<GLchar, Char> Chars;
 
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
 
+	for (GLubyte c = 0; c < 128; c++)
+	{
+		// Load character glyph 
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		// Generate texture
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer
+		);
+		// Set texture options
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// Now store character for later use
+		Char character = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		Chars.insert(std::pair<GLchar, Char>(c, character));
+	}
 
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
 
-
-
-
-
-
+void TextRenderer::RenderText()
+{
+	
+}
--- src/Text.h
+++ src/Text.h
@@ -1,1 +1,26 @@
 #pragma once
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <iostream>
+#include <GLM/glm.hpp>
+#include<GLM/gtc/matrix_transform.hpp>
+#include <map>
+
+struct Char {
+	int TextureID;  // ID handle of the glyph texture
+	glm::vec2 Size;       // Size of glyph
+	glm::vec2 Bearing;    // Offset from baseline to left/top of glyph
+	int Advance;    // Offset to advance to next glyph
+};
+
+class TextRenderer
+{
+	TextRenderer(bool Bold);
+
+	void RenderText();
+	glm::mat4 projection = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
+
+
+};
+
+
